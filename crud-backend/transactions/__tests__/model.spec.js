3822d62d7ff9a4fbc86cb2cf7960c4ff9fd44468
Moana <moanadl@gmail.com>
2025-06-05 17:09:55 -0300

    fixing model

--- crud-backend/transactions/__tests__/model.spec.js
+++ crud-backend/transactions/__tests__/model.spec.js
@@ -6,12 +6,6 @@ import { UserDoesntOwnTransactionError } from "../errors/user-doesnt-own-transac
 
 describe('Transaction model', () => {
 
-    const transactionRepositoryMock = {
-        findByUserUid: () => Promise.resolve([
-            {uid: 'transaction1'}, {uid: 'transaction2'}
-        ])
-    };
-
     describe ('given find user by uid', () => {
 
         test('when user is not informed, then return error 500', async () => {
@@ -19,23 +13,28 @@ describe('Transaction model', () => {
             const response = model.findByUser(); // Chamei o findByUser sem o user
             await expect(response).rejects.toBeInstanceOf(UserNotInformedError);
         
-        })
+        });
 
         test('when user uid is not informed, then return error 500', async () => {
             const model = new Transaction(); 
             model.user = {}; // Tenho um user mas o user não tem uid
             const response = model.findByUser(); // Chamei o findByUser com um user sem uid
             await expect(response).rejects.toBeInstanceOf(UserNotInformedError);
-        })
+        
+        });
 
         test('when user is informed, then return transactions', async () => {
-            const model = new Transaction(transactionRepositoryMock); 
+            const transactions = [{ uid: 'transaction1'}, { uid: 'transaction2' }];
+            const model = new Transaction({
+                findByUserUid: () => Promise.resolve(transactions)
+            }); 
+
             model.user = {uid: 'anyUserUid'};
+
             const response = model.findByUser();
-            await expect(response).resolves.toEqual([
-                {uid: 'transaction1'}, {uid: 'transaction2'}
-            ]);
-        })
+
+            await expect(response).resolves.toEqual(transactions);
+        });
 
     });
 
@@ -61,11 +60,12 @@ describe('Transaction model', () => {
             const model = new Transaction({
                 findByUid: () => Promise.resolve(transactionDB)
             });
+
             model.uid = 9;
             model.user = { uid: 'anyUserUid' };
 
             await expect(model.findByUid()).rejects.toBeInstanceOf(UserDoesntOwnTransactionError);
-        })
+        });
 
         test ('when uid not present, then return error 500', async () => {
             const model = new Transaction();
@@ -80,7 +80,7 @@ describe('Transaction model', () => {
             model.uid = 9;
 
             await expect(model.findByUid()).rejects.toBeInstanceOf(TransactionNotFoundError);
-        })
+        });
 
     });
     
@@ -98,7 +98,15 @@ describe('Transaction model', () => {
             user: {
                 uid: 'anyUserUid'
             }
-        }
+        };
+
+        const repositoryMock = {
+            _hasSaved: false,
+            save () {
+                this._hasSaved = true;
+                return Promise.resolve({ uid: 1 });
+            }
+        };
         
         test ('then return new transaction', async () => {
             const model = new Transaction(repositoryMock);
@@ -108,60 +116,20 @@ describe('Transaction model', () => {
             const newTransaction = createTransaction();
 
             expect(model).toEqual(newTransaction);
-        })
+        });
 
         test ('then save transaction', async () => {
-            
-
             const model = new Transaction(repositoryMock);
 
             await model.create(params);
 
-
-        })
+            expect(repositoryMock._hasSaved).toBeTruthy();
+        });
 
 
     });
 
     describe ('given update transaction', () => {
-
-        let repositoryMock;
-
-        beforeEach(() => {
-            repositoryMock = {
-                _hasUpdated: false,
-                findByUid () {
-                    return Promise.resolve({ user: { uid: 'anyUserUid' } });
-                },
-                update () {
-                    this._hasUpdated = true;
-                    return Promise.resolve();
-                }
-            }
-        })
-        
-        test ('then return update transaction', async () => {
-            const model = new Transaction(repositoryMock);
-            model.uid = 1;
-            model.user = { uid: 'anyUserUid' };
-
-            const params = createTransaction();
-            await model.update(params);
-
-            const updatedTransaction = createTransaction();
-            expect(model).toEqual(updatedTransaction);
-        });
-
-        test ('then update transaction', async () => {
-            const model = new Transaction(repositoryMock);
-            model.uid = 1;
-            model.user = { uid: 'anyUserUid' };
-
-            const params = createTransaction();
-            await model.update(params);
-
-            expect(repositoryMock._hasUpdated).toBeTruthy();
-        });
 
         test ('when transaction doesnt belong to user, then return error', async () => {
             const model = new Transaction({
@@ -183,6 +151,45 @@ describe('Transaction model', () => {
 
             const params = createTransaction();
             await expect(model.update(params)).rejects.toBeInstanceOf(TransactionNotFoundError);
+        });
+
+        describe ('when success', () => {
+
+            let repositoryMock;
+            let model;
+
+            beforeEach(() => {
+                repositoryMock = {
+                    _hasUpdated: false,
+                    findByUid () {
+                        return Promise.resolve({ user: { uid: 'anyUserUid' } });
+                    },
+                    update () {
+                        this._hasUpdated = true;
+                        return Promise.resolve();
+                    }
+                };
+
+                model = new Transaction(repositoryMock);
+                model.uid = 1;
+                model.user = { uid: 'anyUserUid' };
+            });
+
+            test ('then return update transaction', async () => {
+                const params = createTransaction();
+                await model.update(params);
+
+                const updatedTransaction = createTransaction();
+                expect(model).toEqual(updatedTransaction);
+            });
+
+            test ('then update transaction', async () => {
+                const params = createTransaction();
+                await model.update(params);
+
+                expect(repositoryMock._hasUpdated).toBeTruthy();
+            });
+
         });
 
     });
@@ -220,7 +227,6 @@ describe('Transaction model', () => {
             model.user = { uid: 'anyOtherUserUid' };
             
             await expect(model.delete()).rejects.toBeInstanceOf(UserDoesntOwnTransactionError);
-
         });
 
         test ('when transaction doesnt exist, then return error', async () => {
@@ -231,10 +237,9 @@ describe('Transaction model', () => {
             model.user = { uid: 'anyOtherUserUid' };
             
             await expect(model.delete()).rejects.toBeInstanceOf(TransactionNotFoundError);
-
         });
 
-    })
+    });
 
     function createTransaction () {
 
@@ -250,18 +255,9 @@ describe('Transaction model', () => {
             transaction.type = 'income';
             transaction.user = {
                 uid: 'anyUserUid'
-            }
+            };
 
             return transaction;
-    }
+    };
 
-    const repositoryMock = {
-        _hasSaved: false,
-        save() {
-            this._hasSaved = true;
-            return Promise.resolve({ uid: 1 });
-        }
-
-    }
-    
-})
+});
--- crud-backend/transactions/model.js
+++ crud-backend/transactions/model.js
@@ -12,6 +12,7 @@ export class Transaction {
     money;
     transactionType;
     type;
+    uid;
     user;
 
     #repository;
@@ -19,7 +20,7 @@ export class Transaction {
     constructor (transactionRepository) {
         // Caso o argumento 'transactionRepository' tenha sido informado, use ele. Caso contrário use uma instância do TransactionRepository. Isso foi feito para possibilitar o teste
         this.#repository = transactionRepository || new TransactionRepository(); // Vai pegar os dados
-    }
+    };
 
     findByUser () {
 
@@ -27,36 +28,38 @@ export class Transaction {
         // console.log('findByUser - Model,', this.user.uid); // Funciona!!!
         if (!this.user?.uid) {
             return Promise.reject(new UserNotInformedError());
-        }
+        };
 
         // console.log(this.#repository.findByUserId(this.user.uid));
         return this.#repository.findByUserUid(this.user.uid);
        
-    }
+    };
 
     findByUid () {
 
         if (!this.uid) {
             return Promise.reject(new TransactionUidNotInformedError());
-        }
+        };
         // console.log('findByUid - Model');
-        console.log('findByUid - Model,', this.user.uid);
+        //console.log('findByUid - Model,', this.user.uid);
 
         return this.#repository.findByUid(this.uid).then(transactionDB => {
             if (!transactionDB) {
                 return Promise.reject(new TransactionNotFoundError());
             };
+
             if (this.user.uid != transactionDB.user.uid) {
                 return Promise.reject(new UserDoesntOwnTransactionError());
-            }
+            };
+
             this.date = transactionDB.date;
             this.description = transactionDB.description;
             this.money = transactionDB.money;
             this.transactionType = transactionDB.transactionType;
             this.type = transactionDB.type;
             this.user = transactionDB.user;
-        })
-    }
+        });
+    };
 
     create (params) {
         this.date = params.date;
@@ -68,8 +71,8 @@ export class Transaction {
         
         return this.#repository.save(this).then(response => {
             this.uid = response.uid;
-        })
-    }
+        });
+    };
 
     update (params) {
 
@@ -79,18 +82,18 @@ export class Transaction {
             this.money = params.money;
             this.transactionType = params.transactionType;
             this.type = params.type;
-            this.user = params.user;
+            //this.user = params.user;
 
             return this.#repository.update(this);
 
-        })
+        });
         
-    }
+    };
 
     delete () {
         return this.findByUid().then(() => {
             return this.#repository.delete(this);
-        })
-    }
+        });
+    };
 
-}
+};
